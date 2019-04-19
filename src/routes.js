e4337241fe23b762c5233e27ed3c861eb280fa5d
Jaccon <jaccon@gmail.com>
2019-04-19 03:35:12 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,9 @@ const routes = express.Router();
 const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
+routes.get('/boxes/:id', BoxController.show);
 routes.post('/boxes', BoxController.store);
+
 routes.post(
     '/boxes/:id/files',
     multer(multerConfig).single('file'),
