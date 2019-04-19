a8d8c587aab5bc7f847a273534161b5b65ab3736
Jaccon <jaccon@gmail.com>
2019-04-19 03:38:08 -0300

    update BoxController

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,10 +1,20 @@
-const Box = require('../models/Box');
+const Box = require('../models/Box')
 
 class BoxController {
     async store(req, res) {
-        const box = await Box.create({ title: 'Rocket'});
-        return res.json(box);
+        const box = await Box.create(req.body)
+
+        return res.json(box)
+    }
+
+    async show(req, res) {
+        const box = await Box.findById(req.params.id).populate({
+            path: 'files',
+            options: { sort: { createdAt: -1} }
+        })
+
+        return res.json(box)
     }
 }
 
-module.exports = new BoxController();
+module.exports = new BoxController()
