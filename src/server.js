40c414b3b2dd247abe40ace35ba9b6e5ba8d11cd
Jaccon <jaccon@gmail.com>
2019-04-19 03:48:58 -0300

    update servers.js

--- src/server.js
+++ src/server.js
@@ -11,27 +11,30 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+// Socket IO
 io.on('connection', socket => {
     socket.on('connectRoom', box => {
         socket.join(box)
     })
 })
 
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+// Socket IO
+
 // conhecta com o banco de dados Mongo Atlas
 mongoose.connect(app_db_url, {
     useNewUrlParser: true
 }); 
 
-// Socket IO
-app.use((req, res, next) => {
-    req.io = io;
-    return next();
-});
 
-app.use(express.json()); // midleware - possibilita o recebimento de parametros JSON
-app.use(express.urlencoded({extended: true})); // midleware - possibilita o recebimento de arquivos
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
-app.use(require('./routes')); // importa o arquivo de rotas
+app.use(express.json())
+app.use(express.urlencoded( { extended: true } ))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
-server.listen(app_port);
+app.use(require('./routes'))
+
+server.listen(process.env.PORT || 3333)
